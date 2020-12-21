6f66ecdc67ed0bcf6072307930ae5562d49bb69b
Sven Notermans <sven.notermans@live.nl>
2020-12-21 14:47:28 +0100

    finished and working

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBwV3p5sEMeuw0484eGTeCbLzy5Ppzl-Ho",
-    authDomain: "slack-clone-react-6934f.firebaseapp.com",
-    databaseURL: "https://slack-clone-react-6934f.firebaseio.com",
-    projectId: "slack-clone-react-6934f",
-    storageBucket: "slack-clone-react-6934f.appspot.com",
-    messagingSenderId: "1015872541314",
-    appId: "1:1015872541314:web:8dcd2e6e6a090472ac5a9b"
-  };
+  apiKey: "AIzaSyDUHPRxZ7Rg6UFuT7h2Owr1MwCo5xGrlFk",
+  authDomain: "slack-clon-f69b4.firebaseapp.com",
+  projectId: "slack-clon-f69b4",
+  storageBucket: "slack-clon-f69b4.appspot.com",
+  messagingSenderId: "199804672888",
+  appId: "1:199804672888:web:6929c1cb425992f52b468d",
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
   const db = firebaseApp.firestore()
